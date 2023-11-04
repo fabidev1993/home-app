ca5f5830a1ac6787145b55ca522a963d678cf964
fakhbari-dev <f.akhbari72dev@gmail.com>
2023-11-04 13:45:23 +0330

    all added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { lazy, Suspense } from 'react'; // Must be imported for webpack to work
 import './App.css';
 
+const Header = lazy(() => import('HeaderApp/Header'));
+const Fereshteh = lazy(() => import('HeaderApp/Fereshteh'));
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Suspense fallback={<div>Loading Header...</div>}>
+                <Header />
+            </Suspense>
+            <div className="container">Demo home page</div>
+            <Suspense fallback={<div>Loading Header...</div>}>
+                <Fereshteh />
+            </Suspense>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Reza.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reza = () => {
+    return (
+        <div>
+            hello from reza in Header :D
+        </div>
+    );
+};
+
+export default Reza;
